6be2f122263de638e979633a523b546a79eb8fbd
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-12-04 23:34:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Meteor Python Script API
+
+A Meteor Addon that lets you create Meteor modules in Python.
+
+### How do I use it?
+1. Launch up Minecraft Fabric with the Mod installed.
+2. Close the game and go to your .minecraft folder.
+3. Go to the Meteor-Client Folder and then go to Scripts.
+4. Put your Python Script into that folder and then Relaunch the game.
+5. You then should have a Category in the ClickGui that shows your modules.
+
+### How do I code a Python Script for it?
+
+Please use the wiki! That explains almost everything the API has to offer.
+
+But here's an example piece of code
+
+```python
+from net.minecraft.client import MinecraftClient
+from net.minecraft.text import Text
+
+mc = MinecraftClient.getInstance()
+
+def onTickEventPre():
+    mc.player.setSprinting(True)
+
+def onActivate():
+    mc.player.sendMessage(Text.of("Just Enabled Sprint"))
+
+def onDeactivate():
+    mc.player.sendMessage(Text.of("Just Disabled Sprint"))
+
+```
+
+### How does it work?
+It uses Jython's Python to Java library to convert your Python code to Java code. My API then checks for any Event Handlers like (for now) Tick Events, Activate, and Deactivate.
+
+### Credits
+- [Jython](https://www.jython.org/) for their Library.
+- [Walper](https://github.com/ridglef/walper-addon) (Pegasus) for the Inspiration + code I used
+
+
---
+++ src/main/java/uk/cryo/scripts/API.java
@@ -0,0 +1,80 @@
+package uk.cryo.scripts;
+
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.MeteorClient;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.orbit.EventHandler;
+import org.python.util.PythonInterpreter;
+import org.slf4j.Logger;
+
+import java.io.*;
+
+public class API extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category Scripts = new Category("Scripts");
+
+    public static final File SCRIPTS_FOLDER = new File(MeteorClient.FOLDER, "scripts");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Scripts");
+
+        if (!SCRIPTS_FOLDER.exists()) SCRIPTS_FOLDER.mkdir();
+
+        PythonInterpreter translationPython = new PythonInterpreter();
+
+        for (File s : SCRIPTS_FOLDER.listFiles()) {
+            LOG.info("Loading " + s.getName() + " script");
+
+            translationPython.execfile(s.getAbsolutePath());
+
+            Module mod = new Module(Scripts, s.getName().replace(".py", ""), "lol") {
+
+                @Override
+                public void onActivate() {
+                    try {
+                        translationPython.exec("onActivate()");
+                    } catch (Exception ignored) {}
+                }
+
+                @EventHandler
+                private void onTickEventPre(TickEvent.Pre event) {
+                    try {
+                        translationPython.exec("onTickEventPre()");
+                    } catch (Exception ignored) {}
+                }
+
+                @EventHandler
+                private void onTickEventPost(TickEvent.Post event) {
+                    try {
+                        translationPython.exec("onTickEventPost()");
+                    } catch (Exception ignored) {}
+                }
+
+                @Override
+                public void onDeactivate(){
+                    try {
+                        translationPython.exec("onDeactivate()");
+                    } catch (Exception ignored) {}
+                }
+
+            };
+            Modules.get().add(mod);
+
+        }
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(Scripts);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.addon";
+    }
+}
