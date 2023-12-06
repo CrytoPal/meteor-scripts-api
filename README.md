4605e345e9bb6492935ce79d347682c23d6bc002
Cryo <110951096+CrytoPal@users.noreply.github.com>
2023-12-05 19:44:46 -0500

    Added Lua Language Support

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
-# Meteor Python Script API
+# Meteor Multi Language Script API
 
-A Meteor Addon that lets you create Meteor modules in Python.
+A Meteor Addon that lets you create Meteor modules in Multiple Languages.
+
+### What Languages do we support?
+
+We currently only support Python and Lua, more are coming in the future though.
 
 ### How do I use it?
 1. Launch up Minecraft Fabric with the Mod installed.
 2. Close the game and go to your .minecraft folder.
 3. Go to the Meteor-Client Folder and then go to Scripts.
-4. Put your Python Script into that folder and then Relaunch the game.
+4. Put your Python / Lua Script into that folder and then Relaunch the game.
 5. You then should have a Category in the ClickGui that shows your modules.
 
-### How do I code a Python Script for it?
+### How do I code a Script?
 
 Please use the wiki! That explains almost everything the API has to offer.
+
+
+### Python
 
 But here's an example piece of code
 
@@ -32,7 +39,27 @@ def onDeactivate():
 
 ```
 
+### Lua
+
+```lua
+
+function onActivate()
+    print("Hello from Lua!")
+end
+
+function onTickPre()
+     mc.player:setSprinting(true)
+end
+
+function onDeactivate()
+    print("Goodbye from Lua!")
+    mc.player:setSprinting(false)
+end
+```
+
 Although, the code must be obfuscated through minecraft's mappings. For example
+
+### Python
 
 ```python
 
@@ -50,12 +77,30 @@ def onActivate():
 def onDeactivate():
     mc.field_1724.method_43496(class_2561.method_30163("Just Disabled Sprint"))
 ```
+### Lua
+
+```lua
+function onActivate()
+    print("Hello from Lua!")
+end
+
+function onTickPre()
+     mc.field_1724:method_5728(true)
+end
+
+function onDeactivate()
+    print("Goodbye from Lua!")
+    mc.field_1724:method_5728(false)
+end
+```
 
 ### How does it work?
 It uses Jython's Python to Java library to convert your Python code to Java code. My API then checks for any Event Handlers like (for now) Tick Events, Activate, and Deactivate.
+It also uses Luaj to convert your Lua code to Java code and execture it. My API then checks for any Event Handlers like (for now) Tick Events, Activate, and Deactivate.
 
 ### Credits
 - [Jython](https://www.jython.org/) for their Library.
 - [Walper](https://github.com/ridglef/walper-addon) (Pegasus) for the Inspiration + code I used
+- [Luaj](https://github.com/luaj/luaj) for Lua Library
 
 
--- src/main/java/uk/cryo/scripts/API.java
+++ src/main/java/uk/cryo/scripts/API.java
@@ -8,10 +8,16 @@ import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.orbit.EventHandler;
+import org.luaj.vm2.Globals;
+import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
+import org.luaj.vm2.lib.jse.JsePlatform;
 import org.python.util.PythonInterpreter;
 import org.slf4j.Logger;
 
 import java.io.*;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
 
 public class API extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
@@ -27,47 +33,89 @@ public class API extends MeteorAddon {
 
         PythonInterpreter translationPython = new PythonInterpreter();
 
-        byte[] array = new byte[100];
-
         for (File s : SCRIPTS_FOLDER.listFiles()) {
             LOG.info("Loading " + s.getName() + " script");
+            if (s.getName().contains(".py")) {
 
-            translationPython.execfile(s.getAbsolutePath());
+                translationPython.execfile(s.getAbsolutePath());
 
-            Module mod = new Module(Scripts, s.getName().replace(".py", ""), "") {
+                Module mod = new Module(Scripts, s.getName().replace(".py", ""), "") {
 
-                @Override
-                public void onActivate() {
-                    try {
-                        translationPython.exec("onActivate()");
-                    } catch (Exception ignored) {}
+                    @Override
+                    public void onActivate() {
+                        try {
+                            translationPython.exec("onActivate()");
+                        } catch (Exception ignored) {
+                        }
+                    }
+
+                    @EventHandler
+                    public void onTickEventPre(TickEvent.Pre event) {
+                        try {
+                            translationPython.exec("onTickPre()");
+                        } catch (Exception ignored) {
+                        }
+                    }
+
+
+                    @EventHandler
+                    public void onTickEventPost(TickEvent.Post event) {
+                        try {
+                            translationPython.exec("onTickPost()");
+                        } catch (Exception ignored) {
+                        }
+                    }
+
+                    @Override
+                    public void onDeactivate() {
+                        try {
+                            translationPython.exec("onDeactivate()");
+                        } catch (Exception ignored) {
+                        }
+                    }
+
+                };
+                Modules.get().add(mod);
+
+            } else if (s.getName().contains(".lua")) {
+                Globals globals = JsePlatform.standardGlobals();
+                globals.set("mc", CoerceJavaToLua.coerce(mc));
+                globals.set("player", CoerceJavaToLua.coerce(mc.player));
+                Reader reader = null;
+                try {
+                    reader = new BufferedReader(new FileReader(s));
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
                 }
+                LuaValue chunk = globals.load(reader, "script");
+                chunk.call();
+                Module mod = new Module(Scripts, s.getName().replace(".lua", ""), "") {
+                    @Override
+                    public void onActivate() {
+                        LuaValue function = globals.get("onActivate");
+                        if (function.isfunction()) function.call();
+                    }
 
-                @EventHandler
-                public void onTickEventPre(TickEvent.Pre event) {
-                    try {
-                        translationPython.exec("onTickPre()");
-                    } catch (Exception ignored) {}
-                }
+                    @EventHandler
+                    private void onTickEvent(TickEvent.Post event) {
+                        LuaValue function = globals.get("onTickPost");
+                        if (function.isfunction()) function.call();
+                    }
 
+                    @EventHandler
+                    private void onTickEvent(TickEvent.Pre event) {
+                        LuaValue function = globals.get("onTickPre");
+                        if (function.isfunction()) function.call();
+                    }
 
-                @EventHandler
-                public void onTickEventPost(TickEvent.Post event) {
-                    try {
-                        translationPython.exec("onTickPost()");
-                    } catch (Exception ignored) {}
-                }
-
-                @Override
-                public void onDeactivate(){
-                    try {
-                        translationPython.exec("onDeactivate()");
-                    } catch (Exception ignored) {}
-                }
-
-            };
-            Modules.get().add(mod);
-
+                    @Override
+                    public void onDeactivate(){
+                        LuaValue function = globals.get("onDeactivate");
+                        if (function.isfunction()) function.call();
+                    }
+                };
+                Modules.get().add(mod);
+            }
         }
     }
 
